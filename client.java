fa03bd7954a19975b4adcc4f8fff81551aec87bf
Amrutha <96amruthagopinath@gmail.com>
2018-03-24 11:11:09 +0530

    code added

--- client.java
+++ client.java
@@ -5,3 +5,4 @@ public class MyClient {
 public static void main(String[] args) {  
 try{      
 Socket s=new Socket("localhost",6666);
+DataOutputStream dout=new DataOutputStream(s.getOutputStream()); 
